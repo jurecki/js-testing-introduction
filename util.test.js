1b95794df05f3c6a5253467db2a0036668d6ced4
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-12-02 09:59:47 +0100

    Add function checkAndGenerate() and add integration test

--- util.test.js
+++ util.test.js
@@ -1,8 +1,12 @@
-const {generateText} = require('./util');
+const {generateText, checkAndGenerate} = require('./util');
 
 
 test('should output name and age', ()=> {
 	const text = generateText('Max', 29)
 	expect(text).toBe('Max (29 years old)');
-})
+});
 
+test('should generate a valid text output', () => {
+	const text = checkAndGenerate('Max', 29);
+	expect(text).toBe('Max (29 years old)');
+});
